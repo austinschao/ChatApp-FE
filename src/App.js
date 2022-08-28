cfa87c66e7c109a505d0c7bfeab4813421cc5319
Austin Chao <austinschao@gmail.com>
2022-08-28 09:51:37 -0500

    Created separate handle change/submit functions

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [socket, setSocket] = useState(null);
 
+  /** Keep socket inside useEffect to prevent multiple rerenders of the socket */
   useEffect(() => {
     if (socket === null) {
       setSocket(io("http://localhost:3001"));
@@ -26,43 +27,77 @@ function App() {
     }
   }, [socket]);
 
+  // Handle Change Functions //
 
-  function handleChange(evt) {
-    const { name, value } = evt.target;
-    if (name === "enter-username") {
-      setUsername(value);
-    }
-    if (name === "send-message") {
-      setMessage(value);
-    }
-    if (name === "join-room") {
-      setRoom(value);
+  /** Given an input value, change username's state. */
+  function handleUsernameChange(evt) {
+    const { value } = evt.target;
+    setUsername(value);
+  }
+
+  /** Given an input value, change messages's state. */
+  function handleMessageChange(evt) {
+    const { value } = evt.target;
+    setMessage(value);
+  }
+
+  /** Given an input value, change room's state. */
+  function handleRoomChange(evt) {
+    const { value } = evt.target;
+    setRoom(value);
+  }
+
+  // Handle Submit Functions //
+
+  /** If username is not an empty string, setLogged in to true.
+   *
+   *  Console the error.
+  */
+  function handleUsernameSubmit() {
+    if (username) {
+      setLoggedIn(true);
+    } else {
+      console.error("Please add a username");
     }
   }
 
-  function handleSubmit(evt) {
-    const { name } = evt.target;
-    if (name === "enter-username") {
-      setLoggedIn(true);
-    }
-    if (name === "send-message") {
-      setMessages(prev => [...prev, `${username}: ${message}`]);
-      socket.emit("send-message", username, message, currRoom);
-      setMessage("");
-    }
-    if (name === "join-room") {
+  /** If room is not an empty string, setCurrRoom to room and emit it to the socket.
+   *
+   *  Console the error.
+   */
+  function handleJoinRoomSubmit() {
+    if (room) {
       socket.emit("join-room", username, room);
       setLoggedIn(true);
       setCurrRoom(room);
       setRoom("");
+    } else {
+      console.error("Enter a room name");
     }
-    if (name === "leave-room") {
-      socket.emit("leave-room", username, currRoom);
-      setCurrRoom(room);
+  }
+  /** If message is not an empty string, add it to messages and emit it to the socket.
+   *
+   *  Console the error.
+   */
+  function handleMessageSubmit() {
+    if (message) {
+      setMessages(prev => [...prev, `${username}: ${message}`]);
+      socket.emit("send-message", username, message, currRoom);
+      setMessage("");
+    } else {
+      console.error("Enter a message");
     }
   }
 
-
+  /** If currently in a room, leave it. */
+  function handleLeaveRoomSubmit() {
+    if (currRoom) {
+      socket.emit("leave-room", username, currRoom);
+      setCurrRoom(room);
+    } else {
+      console.error("Currently not in a room");
+    }
+  }
 
   return (
     <div className="container">
@@ -71,8 +106,8 @@ function App() {
         ?
         <div>
           <label htmlFor="enter-username">Enter a username: </label>
-          <input type="text" name="enter-username" onChange={handleChange} />
-          <button name="enter-username" onClick={handleSubmit}>Enter Username</button>
+          <input type="text" name="enter-username" onChange={handleUsernameChange} placeholder="Enter a username" />
+          <button name="enter-username" onClick={handleUsernameSubmit}>Enter Username</button>
         </div>
         :
         <div className="messages-container">
@@ -83,16 +118,16 @@ function App() {
           </div>
           <div>
             <label htmlFor="send-message">Message:
-              <input type="text" name="send-message" value={message} onChange={handleChange}
+              <input type="text" name="send-message" value={message} onChange={handleMessageChange} placeholder="Enter a message"
               />
-              <button name="send-message" onClick={handleSubmit}>Send Message</button>
+              <button name="send-message" onClick={handleMessageSubmit}>Send Message</button>
             </label>
           </div>
           <div>
             <label htmlFor="join-room">Room:
-              <input type="text" name="join-room" value={room} onChange={handleChange} />
-              <button name="join-room" onClick={handleSubmit}>Join Room</button>
-              <button name="leave-room" onClick={handleSubmit}>Leave Room</button>
+              <input type="text" name="join-room" value={room} onChange={handleRoomChange} placeholder="Enter a room" />
+              <button name="join-room" onClick={handleJoinRoomSubmit}>Join Room</button>
+              <button name="leave-room" onClick={handleLeaveRoomSubmit}>Leave Room</button>
             </label>
           </div>
         </div>}
